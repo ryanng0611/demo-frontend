cb419ca71179e77ce00c022698178dd4b19b182d
Snappymob <snappymob@Snappymobs-MacBook-Air.local>
2024-02-27 16:25:35 +0800

    CPROPS: Create InputField.vue to practice v-model v-show conecpts

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <InputField />
   </div>
 </template>
+
+<script>
+import InputField from "../components/InputField.vue";
+
+export default {
+  components: {
+    InputField,
+  },
+};
+</script>
