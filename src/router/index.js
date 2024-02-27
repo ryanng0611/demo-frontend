d0adb7330022bf111bb43b4ae4af86c0979cbfc1
Snappymob <snappymob@Snappymobs-MacBook-Air.local>
2024-02-27 14:53:30 +0800

    COUNTER: Implement simple counter component using Class Components

--- src/router/index.js
+++ src/router/index.js
@@ -19,6 +19,11 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
+  {
+    path: "/counter",
+    name: "counter",
+    component: () => import("../views/CounterView.vue"),
+  },
 ];
 
 const router = new VueRouter({
