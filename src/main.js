bd8eeb47d85ecddca89b2a3b00cb467217bfed5d
Snappymob <snappymob@Snappymobs-MacBook-Air.local>
2024-02-28 10:31:33 +0800

    PROPS: Perform code cleanup

--- src/main.js
+++ src/main.js
@@ -4,9 +4,7 @@ import router from "./router";
 import store from "./store";
 import FooterComponent from "./components/FooterComponent.vue";
 import { library } from "@fortawesome/fontawesome-svg-core";
-
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-
 import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
 
 library.add(faUserSecret);
