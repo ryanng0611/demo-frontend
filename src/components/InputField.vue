b201b05ab7070cefa505aa73b0524eb78c44fd68
Snappymob <snappymob@Snappymobs-MacBook-Air.local>
2024-02-28 14:23:04 +0800

    EVENTS: Add event to change company name via input filed

--- src/components/InputField.vue
+++ src/components/InputField.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <input v-model="input" />
-    <p v-show="showTxt">{{ augInput }}</p>
+    <!-- <p v-show="showTxt">{{ augInput }}</p> -->
+    <button v-on:click="changeTitle">Change name</button>
   </div>
 </template>
 
@@ -9,16 +10,34 @@
 import Vue from "vue";
 import Component from "vue-class-component";
 
+const MessageProp = Vue.extend({
+  props: {
+    compName: String,
+  },
+});
+
 @Component
-export default class InputField extends Vue {
-  input = "";
+export default class InputField extends MessageProp {
+  input = this.compName;
 
   get showTxt() {
     return this.input.length > 0;
   }
 
+  get getInput() {
+    return this.input;
+  }
+
+  get getCompName() {
+    return this.compName;
+  }
+
   get augInput() {
     return this.input + (this.showTxt ? ", funny times" : "");
+  }
+
+  changeTitle() {
+    this.$emit("changeTitle", this.input);
   }
 }
 </script>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <InputField />
+    <InputField
+      v-bind:compName="this.compName"
+      v-on:changeTitle="updateTitle($event)"
+    />
+    <h1 v-on:click="changeComp">{{ comp }}</h1>
   </div>
 </template>
 
@@ -9,8 +13,27 @@
 import InputField from "../components/InputField.vue";
 
 export default {
+  data() {
+    return {
+      comp: String,
+    };
+  },
   components: {
     InputField,
   },
+  props: {
+    compName: String,
+  },
+  methods: {
+    updateTitle: function (updatedTitle) {
+      this.comp = updatedTitle;
+    },
+
+    changeComp: function () {
+      this.$emit("changeComp", this.comp);
+    },
+  },
 };
+
+// console.log(this.compName);
 </script>
