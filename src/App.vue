99229fe6d1996f16aecf4b415e021ae57bd1b5fd
Snappymob <snappymob@Snappymobs-MacBook-Air.local>
2024-02-28 16:30:06 +0800

    Add app-header component into root component file

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <app-header :companyName="compName" />
     <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
