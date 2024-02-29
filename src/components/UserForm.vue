c87f5531299a685e75c288ca1cd2bef42f21650b
Snappymob <snappymob@Snappymobs-MacBook-Air.local>
2024-02-29 10:44:15 +0800

    USER-FORM: Create placeholder UserForm and UserFormView Class Components

---
+++ src/components/UserForm.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p>User Register Form</p>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component()
+export default class UserForm extends Vue {}
+</script>
