566054780919f7560978552859111f57b36bca52
Snappymob <snappymob@Snappymobs-MacBook-Air.local>
2024-02-29 11:04:18 +0800

    USER-FORM: Create simple input fields for user form

--- src/components/UserForm.vue
+++ src/components/UserForm.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    <p>User Register Form</p>
+  <div id="user-register-form">
+    <h1>User Register Form</h1>
+    <hr />
+    <form>
+      <label>Username: </label>
+      <input type="text" required />
+      <label>Password: </label>
+      <input type="password" required />
+    </form>
   </div>
 </template>
 
@@ -11,3 +18,27 @@ import Component from "vue-class-component";
 @Component()
 export default class UserForm extends Vue {}
 </script>
+
+<style scoped>
+#user-register-form * {
+  box-sizing: border-box;
+}
+
+#user-register-form {
+  margin: 20px auto;
+  max-width: 500px;
+}
+
+input[type="text"],
+input[type="password"] {
+  display: block;
+  width: 100%;
+  padding: 8px;
+}
+
+label {
+  display: block;
+  margin: 20px 0 10px;
+  text-align: left;
+}
+</style>
